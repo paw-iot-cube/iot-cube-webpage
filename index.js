0ab0808eb542425a8f295631d81241d443db186a
Viperinius <viperinius@gmx.de>
2019-05-20 01:16:28 +0200

    changed to first Dashboard layout and first tests with nodered interaction

--- index.js
+++ index.js
@@ -65,17 +65,6 @@ $( document ).ready(function() {
     // Turn on debugging for uibuilderfe (default is off)
     uibuilder.debug(true);
 
-    // If msg changes - msg is updated when a standard msg is received from Node-RED over Socket.IO
-    // Note that you can also listen for 'msgsReceived' as they are updated at the same time
-    // but newVal relates to the attribute being listened to.
-    uibuilder.onChange('msg', function(newVal){
-        console.info('indexjs:msg: property msg has changed! ', newVal);
-        $('#showMsg').text(JSON.stringify(newVal));
-        //uibuilder.set('msgCopy', newVal)
-        
-        $('#latestPayload').text(newVal.payload);
-    });
-
     // You can get attributes manually. Non-existent attributes return 'undefined'
     //console.dir(uibuilder.get('msg'))
 
@@ -97,192 +86,117 @@ $( document ).ready(function() {
         console.info('indexjs:msgsCtrl: New control msg sent to us from Node-RED over Socket.IO. Total Count: ', newVal);
     });
     
-    //------------------------------------------------------------------
-    $('#list').click(function(event){
-        event.preventDefault();
-        $('#elements .item').addClass('list-group-item');
-    });
-    $('#grid').click(function(event) {
-        event.preventDefault();
-        $('#elements .item').removeClass('list-group-item');
-        $('#elements .item').addClass('grid-group-item');
-    });
+    // If msg changes - msg is updated when a standard msg is received from Node-RED over Socket.IO
+    // Note that you can also listen for 'msgsReceived' as they are updated at the same time
+    // but newVal relates to the attribute being listened to.
+    var test = false;
 
-    var modalAddCaller;
-    var modalDelCaller;
-
-    $(document).on('shown.bs.modal', '#modalAdd', function(event) {
-        //get element that triggered the modal
-        var triggerElement = $(event.relatedTarget);
-        modalAddCaller = triggerElement[0].parentElement.parentElement;
-        console.log(triggerElement[0].parentElement.parentElement);
-    });
-    
-    $(document).on('shown.bs.modal', '#modalDelete', function(event) {
-        //get element that triggered the modal
-        var triggerElement = $(event.relatedTarget);
-        modalDelCaller = triggerElement[0].parentElement.parentElement.parentElement.parentElement.parentElement;
-        console.log(triggerElement[0].parentElement.parentElement.parentElement.parentElement.parentElement);
-    });
-
-    // modal type dropdown
-    $('.dropdown-menu li a').click(function() {
-        $(this).parents('.dropdown').find('.btn').html($(this).text() + ' <span class="caret"></span>');
-        $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
-    });
-
-    // handle modal submit
-    $('#modalForm').on('submit', function(e) {
-        e.preventDefault();
-        var nameVal = $('#formName').val();
-        var typeVal = $('#dropdownType').val();
-        if (typeVal === "") {
-            //no type specified
-            alert("Please choose a sensor type");
-            return;
+    uibuilder.onChange('msg', function(newVal) {
+        console.info('indexjs:msg: property msg has changed! ', newVal);
+        console.log(newVal);
+        if (!test) {
+            makeChart(newVal, 'chLine', '#007bff');
+            test = true;
+            console.log("new chart");
         }
-        var delayVal = $('#formDelay').val();
-        var gpioVal = $('#formGpio').val();
-        var ipVal = $('#formIp').val();
-
-        var collection = {
-            'nameVal': nameVal,
-            'typeVal': typeVal,
-            'delayVal': delayVal,
-            'gpioVal': gpioVal,
-            'ipVal': ipVal
-        };
-        uibuilder.send({'topic':'modalDataAdd', 'payload': collection});       
-
-        //close modal
-        $('#modalAdd').modal('hide');
-        
-        //replace empty element with completed one
-        var completeTemplate = $('#templateFullElem').html();
-        completeTemplate = completeTemplate
-            .replace('%TITLE%', nameVal)
-            .replace('%DELAY%', delayVal)
-            .replace('%GPIO%', gpioVal)
-            .replace('%IP%', ipVal);
-        if (typeVal != "sensorOther") {
-            switch (typeVal) {
-                case 'sensorDHT11':
-                    completeTemplate = completeTemplate.replace('sensor.jpg', 'static/dht11.jpg');
-                    break;
-                case 'sensorBME280':
-                    completeTemplate = completeTemplate.replace('sensor.jpg', 'static/bme280.jpg');
-                    break;
-                case 'sensorHCSR501':
-                    completeTemplate = completeTemplate.replace('sensor.jpg', 'static/hcsr501.jpg');
-                    break;
-                case 'sensorButton':
-                    completeTemplate = completeTemplate.replace('sensor.jpg', 'static/button.jpg');
-                    break;
-            
-                default:
-                    break;
-            }
+        else {
+            addToLineChart(lineChart, newVal, '#007bff');
+            console.log("add/update");
         }
 
-
-        $(modalAddCaller).replaceWith($(completeTemplate).clone());
-        addDragEventListeners();    //recall listener adding to make new elements draggable
-    });
-    
-    $('#modalDelConfirm').click(function() {
-        uibuilder.send({'topic': 'modalDel', 'payload':'THIS NEEDS TO BE CHANGED'});
         
-        //close modal
-        $('#modalDelete').modal('hide');
-        
-        //replace complete element with empty one
-        var emptyTemplate = $('#templateEmptyElem').html();
-        $(modalDelCaller).replaceWith($(emptyTemplate).clone());
-        addDragEventListeners();    //recall listener adding to make new elements draggable
     });
-    
-    // add empty elements
-    $('button.addRow').click(function(){
-        var emptyTemplate = $('#templateEmptyElem').html();
-        addRowFunc(emptyTemplate);
-    });
-
-    function addRowFunc(template) {
-        var item = $(template).clone();
-        $('#elements').append(item);
-        addDragEventListeners();    //recall listener adding to make new elements draggable
-    }
     
 }); // --- End of JQuery Ready --- //
 
 
-//++++++++++++++++++++++++++++++++++++++++++++Drag-and-Drop++++++++++++++++++++++++++++++++++++++++++++
+var lineChart;
 
-var dragSrcEl = null;
-
-function handleDragStart(e) {
-    this.style.opacity = '0.4';  // this / e.target is the source node.
-    dragSrcEl = this;
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-function handleDragOver(e) {
-    if (e.preventDefault) {
-        e.preventDefault(); // Necessary. Allows us to drop.
-    }
-
-    e.dataTransfer.dropEffect = 'move';
-
-    return false;
-}
-
-function handleDragEnter(e) {
-    // this / e.target is the current hover target.
-    this.classList.add('over');
-}
-
-function handleDragLeave(e) {
-    this.classList.remove('over');  // this / e.target is previous target element.
-}
-
-function handleDrop(e) {
-    // this / e.target is current target element.
-
-    if (e.stopPropagation) {
-        e.stopPropagation(); // stops the browser from redirecting.
-    }
-
-    if (dragSrcEl != this) {
-        // Set the source column's HTML to the HTML of the columnwe dropped on.
-        dragSrcEl.innerHTML = this.innerHTML;
-        this.innerHTML = e.dataTransfer.getData('text/html');
-    }
-
-    return false;
-}
-
-function handleDragEnd(e) {
-    // this/e.target is the source node.
-    this.style.opacity = '1.0';
-
-    var cols = document.querySelectorAll('#elements .item .thumbnail');
-    [].forEach.call(cols, function (col) {
-        col.classList.remove('over');
+function makeChart(msgObject, canvasId, colour) {
+    // chart
+    var canvas = $('#' + canvasId);
+    
+    
+    //var inputTitle = "Humidity history";
+    //var inputTimes = [1558121936, 1558121937, 1558121938, 1558121939, 1558121940];
+    //var inputData = [30, 31, 33.5, 30.2, 29.4];
+    var inputTitle = msgObject.chart.title;
+    var inputTimes = msgObject.chart.times;
+    var inputData = msgObject.chart.data;
+    var inputLabels = [];
+    
+    var timeFormat =  'HH:mm:ss';
+    console.log(inputData);
+    
+    //generate displayed time
+    $.each(inputTimes, function (indexInArray, valueOfElement) {
+        inputLabels.push(moment.unix(valueOfElement).format(timeFormat));
     });
+    
+    // for testing: moment.unix(1558121936).format(timeFormat) with timeFormat = 'YYYY-MM-DD HH:mm:ss'
+    
+    var chartData = {
+        labels: inputLabels,
+        datasets: [{
+            data: inputData,
+            borderColor: colour,
+            borderWidth: 4,
+            pointBackgroundColor: colour
+        }]
+    };
+    
+    if (canvas) {
+        lineChart = new Chart(canvas, {
+            type: 'line',
+            data: chartData,
+            options: {
+                title: {
+                    display: true,
+                    text: inputTitle
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: false
+                        }
+                    }]
+                },
+                legend: {
+                    display: false
+                }
+            }
+        });
+    }
 }
 
-function addDragEventListeners(e) {
-    var cols = document.querySelectorAll('#elements .item .thumbnail');
-    [].forEach.call(cols, function(col) {
-        col.addEventListener('dragstart', handleDragStart, false);
-        col.addEventListener('dragenter', handleDragEnter, false);
-        col.addEventListener('dragover', handleDragOver, false);
-        col.addEventListener('dragleave', handleDragLeave, false);
-        col.addEventListener('drop', handleDrop, false);
-        col.addEventListener('dragend', handleDragEnd, false);
+function addToLineChart(chart, msgObject, colour) {
+    var inputTimes = msgObject.chart.times;
+    var inputData = msgObject.chart.data;
+    var inputLabels = [];
+
+    console.log(inputData);
+    
+    var timeFormat =  'HH:mm:ss';
+    
+    //generate displayed time
+    $.each(inputTimes, function (indexInArray, valueOfElement) {
+        inputLabels.push(moment.unix(valueOfElement).format(timeFormat));
     });
+    
+    console.log(chart.data.labels);
+    
+    chart.data.labels = [];
+    inputLabels.forEach((label) => {
+        chart.data.labels.push(label);
+    });
+    chart.data.datasets.pop();
+    chart.data.datasets.push({
+            label: inputLabels,
+            data: inputData,
+            borderColor: colour,
+            borderWidth: 4,
+            pointBackgroundColor: colour
+    });
+
+    chart.update();
 }
-
-addDragEventListeners();
-
